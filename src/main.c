4a481571a5a4b0827e800f4f4c21b58dbc8329bd
tg201005 <teemo20001005@gmail.com>
2023-05-13 15:35:54 +0900

    feat : apply parsing.c to main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyi <tyi@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/13 15:26:30 by tyi               #+#    #+#             */
+/*   Updated: 2023/05/13 15:28:24 by tyi              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "execute.h"
 
-int main(void)
-{
-	t_info info;
+void	parsing(t_info *info, char *path);
 
+int	main(int ac, char **av)
+{
+	t_info	info;
+
+	if (ac != 2)
+		exit(1);
+	parsing(&info, av[1]);
 	executer(&info);
 	return (0);
 }
